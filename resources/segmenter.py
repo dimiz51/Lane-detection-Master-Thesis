49aae4a38f3e53e20e52f7fa0a6a001cd31b2f7a
DimitrisZarogiannis <dimitriszarogiannis461@gmail.com>
2023-03-22 02:31:13 +0100

    +roc curve

--- resources/segmenter.py
+++ resources/segmenter.py
@@ -68,7 +68,7 @@ def train(model, train_loader, val_loader = None, num_epochs=10, lr=0.001, momen
             train_loss += loss.item() * inputs.size(0)
             train_iou += iou_score(eval_out.to(device).detach(), targets)
             train_f1 += f1_score(eval_out.to(device).detach(),targets)
-            
+        
         if val_loader:
             for batch_idx, (inputs, targets) in enumerate(val_loader): 
                 model.eval()
@@ -97,16 +97,17 @@ def train(model, train_loader, val_loader = None, num_epochs=10, lr=0.001, momen
             print('Epoch: {} - Train Loss: {:.4f} - Train_IoU: {:.5f} - Train_F1: {:.5f}'.format(epoch+1, train_loss, train_iou, train_f1))
             
             
-# Segmenter pipeline class (ViT + Masks transformer end-to-end)
+# Segmenter pipeline class (ViT + Masks predictor end-to-end)
 class Segmenter(nn.Module):
-    def __init__(self,encoder, decoder, image_size = (640,640), output_act = nn.Sigmoid()):
+    def __init__(self,encoder, decoder, image_size = (640,640), output_act = nn.Sigmoid(), find_threshold = False):
         super().__init__()
         self.patch_size = encoder.patch_size
         self.encoder = encoder
         self.decoder = decoder
         self.image_size = image_size
-        self.lane_threshold = 0.5
+        self.lane_threshold = 0
         self.output_act = output_act
+        self.roc_flag = find_threshold
         
         
     # Forward pass of the pipeline
@@ -118,6 +119,7 @@ class Segmenter(nn.Module):
         
         # Pass through the masks transformer
         masks = self.decoder(x)
+        
 
         # Interpolate patch level class annotatations to pixel level and transform to original image size
         masks = F.interpolate(masks, size=(H, W), mode="bilinear")
@@ -126,15 +128,23 @@ class Segmenter(nn.Module):
         if self.training:
             act = self.output_act
             class_prob_masks = act(masks)
-            predictions = torch.where(class_prob_masks > self.lane_threshold, torch.ones_like(class_prob_masks), torch.zeros_like(class_prob_masks))
+            predictions = torch.where(class_prob_masks > self.lane_threshold,torch.zeros_like(class_prob_masks),torch.ones_like(class_prob_masks))
             return masks, predictions
-        # Evaluation time
+        
+        elif self.roc_flag and not self.training:
+            act = self.output_act
+            class_prob_masks = act(masks)
+            return class_prob_masks
         else:
             act = self.output_act
             class_prob_masks = act(masks)
-            predictions = torch.where(class_prob_masks > self.lane_threshold, torch.ones_like(class_prob_masks), torch.zeros_like(class_prob_masks))
+            predictions = torch.where(class_prob_masks > self.lane_threshold,torch.zeros_like(class_prob_masks),torch.ones_like(class_prob_masks))
             return predictions
         
     # Count pipeline trainable parameters
     def count_parameters(self):
         return sum(p.numel() for p in self.parameters() if p.requires_grad)
+    
+    # Load trained model
+    def load_segmenter(self,path):
+        self.load_state_dict(torch.load(path,map_location=torch.device('cpu')))
--- resources/utils.py
+++ resources/utils.py
@@ -4,6 +4,8 @@ import cv2
 import torch
 from torchmetrics import ConfusionMatrix
 from einops import rearrange
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
     
     
 # Helper func to transform back to array from tensor
@@ -53,3 +55,58 @@ def dice_loss(outputs, targets, smooth=1e-7):
     dice = (2 * intersection + smooth) / (union + smooth)
     loss = 1 - dice
     return loss
+
+
+# ROC AUC Curve to find binary mask optimal threshold
+def find_threshold(prob_preds,gt_mask):
+    
+    prob_arr = prob_preds.detach().cpu().numpy()
+    gt_arr = gt_mask.detach().cpu().numpy()
+    
+    
+    
+    # Calculate ROC curve
+    fpr, tpr, thresholds = roc_curve(gt_arr.ravel(),prob_arr.ravel())
+    roc_auc = auc(fpr, tpr)
+    
+    # Plot ROC curve
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
+    
+    # Find best threshold
+    optimal_idx = np.argmax(tpr - fpr)
+    optimal_threshold = thresholds[optimal_idx]
+    
+    
+    # Print best threshold
+    print(f"The optimal threshold is: {optimal_threshold}")
+    
+    return optimal_threshold
+
+# Get ROC Curve inputs from test set (needs a trained model)
+def get_roc_probs(model,test_loader):      
+    batch_probs = []
+    batch_gt = []
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   
+    if model.roc_flag:
+        model.eval()
+        with torch.no_grad():  # disable gradient calculation during inference
+            for batch_idx, (inputs, targets) in enumerate(test_loader): 
+                inputs, targets = inputs.to(device), targets.to(device)
+                outputs = model(inputs)
+                batch_probs.append(outputs)
+                batch_gt.append(targets)
+            
+        pred_probs = torch.cat(batch_probs, dim=0)
+        ground_truths = torch.cat(batch_gt, dim=0)
+        return pred_probs, ground_truths
+    else:
+        return 1
