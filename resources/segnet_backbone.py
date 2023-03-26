0b3525108e64c4bd44d6d9e1fbcb9ec386dea101
DimitrisZarogiannis <dimitriszarogiannis461@gmail.com>
2023-03-26 19:23:50 +0200

    FUCK SEGMENTER

---
+++ resources/segnet_backbone.py
@@ -0,0 +1,273 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchmetrics import F1Score,JaccardIndex
+from torchvision import transforms
+import torch.optim as optim
+
+
+
+# Custom training function for the transformer pipeline with schedule and augmentations
+def train(model, train_loader, val_loader = None, num_epochs=10, lr=0.01, weight_decay=0, SGD_momentum = 0.9, lr_scheduler=False, lane_weight = None):
+    # Set up loss function and optimizer
+    criterion =  nn.BCEWithLogitsLoss(pos_weight= lane_weight)
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=SGD_momentum, weight_decay=weight_decay)
+
+    # Set up learning rate scheduler
+    if lr_scheduler:
+        pass
+
+    # Set up device (GPU or CPU)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+    
+    f1_score = F1Score(task="binary").to(device)
+    iou_score = JaccardIndex(task= 'binary').to(device)
+    
+    train_augmentations = transforms.Compose([transforms.RandomRotation(degrees=(10, 30)),
+                                              transforms.RandomHorizontalFlip()])
+    
+    # Set a seed for augmentations
+    torch.manual_seed(42) 
+    
+    # Train the model
+    for epoch in range(num_epochs):
+        train_loss = 0
+        train_iou = 0
+        train_f1 = 0
+        
+        val_iou = 0
+        val_f1 = 0
+        
+        for batch_idx, (inputs, targets) in enumerate(train_loader):
+            model.train()
+            # inputs, targets = inputs.to(device), targets.to(device)
+            
+            # Combine the inputs and targets into a single tensor
+            data = torch.cat((inputs, targets), dim=1)
+    
+            
+            # Apply the same augmentations to the combined tensor
+            augmented_data = train_augmentations(data)    
+    
+            # Split the augmented data back into individual inputs and targets
+            inputs = augmented_data[:, :inputs.size(1)].to(device)
+            targets = augmented_data[:, inputs.size(1):].to(device)
+      
+            optimizer.zero_grad()
+            outputs, eval_out = model(inputs)
+
+            loss = criterion(outputs.to(device), targets)
+            loss.backward()
+            optimizer.step()
+            
+            train_loss += loss.item() * inputs.size(0)
+            train_iou += iou_score(eval_out.to(device).detach(), targets)
+            train_f1 += f1_score(eval_out.to(device).detach(),targets)
+        
+        if val_loader:
+            model.eval()
+            with torch.no_grad():
+                for batch_idx, (inputs, targets) in enumerate(val_loader): 
+                
+                    inputs, targets = inputs.to(device), targets.to(device)
+                    logits, outputs = model(inputs)
+                
+                    val_iou += iou_score(outputs.to(device), targets)
+                    val_f1 += f1_score(outputs.to(device),targets)
+        
+                val_iou /= len(val_loader)
+                val_f1 /= len(val_loader)
+            
+        train_loss /= len(train_loader)
+        train_iou /= len(train_loader)
+        train_f1 /= len(train_loader)
+        
+        
+        
+     # Print progress
+        if lr_scheduler:
+            print('Epoch: {} - Train Loss: {:.4f} - Learning Rate: {:.6f} - Train_IoU: {:.5f} - Train_F1: {:.5f}'.format(epoch+1, train_loss,optimizer.param_groups[0]['lr'], train_iou, train_f1))
+            # scheduler.step()
+            if val_loader:
+                print('Val_F1: {:.5f}  - Val_IoU: {:.5f} '.format(val_f1,val_iou))
+        else:
+            print('Epoch: {} - Train Loss: {:.4f} - Train_IoU: {:.5f} - Train_F1: {:.5f}'.format(epoch+1, train_loss, train_iou, train_f1))
+            
+            
+            
+class SegNet(nn.Module):
+
+    def __init__(self, in_chn=3, out_chn=1, BN_momentum=0.5):
+        super(SegNet, self).__init__()
+
+        #SegNet Architecture
+        #Takes input of size in_chn = 3 (RGB images have 3 channels)
+        #Outputs size label_chn (N # of classes)
+
+        #ENCODING consists of 5 stages
+        #Stage 1, 2 has 2 layers of Convolution + Batch Normalization + Max Pool respectively
+        #Stage 3, 4, 5 has 3 layers of Convolution + Batch Normalization + Max Pool respectively
+
+        #General Max Pool 2D for ENCODING layers
+        #Pooling indices are stored for Upsampling in DECODING layers
+
+        self.lane_threshold = 0.5
+        self.in_chn = in_chn
+        self.out_chn = out_chn
+
+        self.MaxEn = nn.MaxPool2d(2, stride=2, return_indices=True) 
+
+        self.ConvEn11 = nn.Conv2d(self.in_chn, 64, kernel_size=3, padding=1)
+        self.BNEn11 = nn.BatchNorm2d(64, momentum=BN_momentum)
+        self.ConvEn12 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.BNEn12 = nn.BatchNorm2d(64, momentum=BN_momentum)
+
+        self.ConvEn21 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.BNEn21 = nn.BatchNorm2d(128, momentum=BN_momentum)
+        self.ConvEn22 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.BNEn22 = nn.BatchNorm2d(128, momentum=BN_momentum)
+
+        self.ConvEn31 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.BNEn31 = nn.BatchNorm2d(256, momentum=BN_momentum)
+        self.ConvEn32 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.BNEn32 = nn.BatchNorm2d(256, momentum=BN_momentum)
+        self.ConvEn33 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.BNEn33 = nn.BatchNorm2d(256, momentum=BN_momentum)
+
+        self.ConvEn41 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.BNEn41 = nn.BatchNorm2d(512, momentum=BN_momentum)
+        self.ConvEn42 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.BNEn42 = nn.BatchNorm2d(512, momentum=BN_momentum)
+        self.ConvEn43 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.BNEn43 = nn.BatchNorm2d(512, momentum=BN_momentum)
+
+        self.ConvEn51 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.BNEn51 = nn.BatchNorm2d(512, momentum=BN_momentum)
+        self.ConvEn52 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.BNEn52 = nn.BatchNorm2d(512, momentum=BN_momentum)
+        self.ConvEn53 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.BNEn53 = nn.BatchNorm2d(512, momentum=BN_momentum)
+
+
+        #DECODING consists of 5 stages
+        #Each stage corresponds to their respective counterparts in ENCODING
+
+        #General Max Pool 2D/Upsampling for DECODING layers
+        self.MaxDe = nn.MaxUnpool2d(2, stride=2) 
+
+        self.ConvDe53 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.BNDe53 = nn.BatchNorm2d(512, momentum=BN_momentum)
+        self.ConvDe52 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.BNDe52 = nn.BatchNorm2d(512, momentum=BN_momentum)
+        self.ConvDe51 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.BNDe51 = nn.BatchNorm2d(512, momentum=BN_momentum)
+
+        self.ConvDe43 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.BNDe43 = nn.BatchNorm2d(512, momentum=BN_momentum)
+        self.ConvDe42 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.BNDe42 = nn.BatchNorm2d(512, momentum=BN_momentum)
+        self.ConvDe41 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
+        self.BNDe41 = nn.BatchNorm2d(256, momentum=BN_momentum)
+
+        self.ConvDe33 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.BNDe33 = nn.BatchNorm2d(256, momentum=BN_momentum)
+        self.ConvDe32 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.BNDe32 = nn.BatchNorm2d(256, momentum=BN_momentum)
+        self.ConvDe31 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
+        self.BNDe31 = nn.BatchNorm2d(128, momentum=BN_momentum)
+
+        self.ConvDe22 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.BNDe22 = nn.BatchNorm2d(128, momentum=BN_momentum)
+        self.ConvDe21 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
+        self.BNDe21 = nn.BatchNorm2d(64, momentum=BN_momentum)
+
+        self.ConvDe12 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.BNDe12 = nn.BatchNorm2d(64, momentum=BN_momentum)
+        self.ConvDe11 = nn.Conv2d(64, self.out_chn, kernel_size=3, padding=1)
+        self.BNDe11 = nn.BatchNorm2d(self.out_chn, momentum=BN_momentum)
+    
+    # Count pipeline trainable parameters
+    def count_parameters(self):
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
+
+    def forward(self, x):
+
+        #ENCODE LAYERS
+        #Stage 1
+        x = F.relu(self.BNEn11(self.ConvEn11(x))) 
+        x = F.relu(self.BNEn12(self.ConvEn12(x))) 
+        x, ind1 = self.MaxEn(x)
+        size1 = x.size()
+
+        #Stage 2
+        x = F.relu(self.BNEn21(self.ConvEn21(x))) 
+        x = F.relu(self.BNEn22(self.ConvEn22(x))) 
+        x, ind2 = self.MaxEn(x)
+        size2 = x.size()
+
+        #Stage 3
+        x = F.relu(self.BNEn31(self.ConvEn31(x))) 
+        x = F.relu(self.BNEn32(self.ConvEn32(x))) 
+        x = F.relu(self.BNEn33(self.ConvEn33(x)))   
+        x, ind3 = self.MaxEn(x)
+        size3 = x.size()
+
+        #Stage 4
+        x = F.relu(self.BNEn41(self.ConvEn41(x))) 
+        x = F.relu(self.BNEn42(self.ConvEn42(x))) 
+        x = F.relu(self.BNEn43(self.ConvEn43(x)))   
+        x, ind4 = self.MaxEn(x)
+        size4 = x.size()
+
+        #Stage 5
+        x = F.relu(self.BNEn51(self.ConvEn51(x))) 
+        x = F.relu(self.BNEn52(self.ConvEn52(x))) 
+        x = F.relu(self.BNEn53(self.ConvEn53(x)))   
+        x, ind5 = self.MaxEn(x)
+        size5 = x.size()
+
+        #DECODE LAYERS
+        #Stage 5
+        x = self.MaxDe(x, ind5, output_size=size4)
+        x = F.relu(self.BNDe53(self.ConvDe53(x)))
+        x = F.relu(self.BNDe52(self.ConvDe52(x)))
+        x = F.relu(self.BNDe51(self.ConvDe51(x)))
+
+        #Stage 4
+        x = self.MaxDe(x, ind4, output_size=size3)
+        x = F.relu(self.BNDe43(self.ConvDe43(x)))
+        x = F.relu(self.BNDe42(self.ConvDe42(x)))
+        x = F.relu(self.BNDe41(self.ConvDe41(x)))
+
+        #Stage 3
+        x = self.MaxDe(x, ind3, output_size=size2)
+        x = F.relu(self.BNDe33(self.ConvDe33(x)))
+        x = F.relu(self.BNDe32(self.ConvDe32(x)))
+        x = F.relu(self.BNDe31(self.ConvDe31(x)))
+
+        #Stage 2
+        x = self.MaxDe(x, ind2, output_size=size1)
+        x = F.relu(self.BNDe22(self.ConvDe22(x)))
+        x = F.relu(self.BNDe21(self.ConvDe21(x)))
+
+        #Stage 1
+        x = self.MaxDe(x, ind1)
+        x = F.relu(self.BNDe12(self.ConvDe12(x)))
+        x = self.ConvDe11(x)
+        
+        probs = F.sigmoid(x)
+        
+        return x,probs
+
+    # Make a single prediction 
+    def predict(self,x):
+        self.eval()
+        probs = self.forward(x)
+        prediction = torch.where(probs > self.lane_threshold, torch.ones_like(probs), torch.zeros_like(probs))
+        return prediction
+        
+    # Load trained model weights
+    def load_weights(self,path): 
+        self.load_state_dict(torch.load(path,map_location=torch.device('cpu')))
+        print('Loaded state dict succesfully!')
